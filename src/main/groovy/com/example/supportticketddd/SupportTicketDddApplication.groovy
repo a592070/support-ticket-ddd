67fb1b15759f1367b330b10e6e34770c821c87b7
gaga <a592070@gmail.com>
2023-08-11 10:27:43 +0800

    initial

---
+++ src/main/groovy/com/example/supportticketddd/SupportTicketDddApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.supportticketddd
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SupportTicketDddApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SupportTicketDddApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/supportticketddd/SupportTicketDddApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.supportticketddd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SupportTicketDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
